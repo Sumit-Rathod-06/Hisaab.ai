ea691aa30e18c4506165e64e0a5f2afa7b049b4b
Sumit-Rathod-06 <sumitrrathod06@gmail.com>
2026-01-10 20:52:44 +0530

    Added database functionality

--- server/Server.js
+++ server/Server.js
@@ -5,12 +5,13 @@ import financeRoutes from "./src/routes/financeRoutes.js";
 
 import expense_router from "./src/routes/expense.router.js";
 import profile_router from "./src/routes/profile.router.js";
+import protect from "./src/middlewares/auth.middlewares.js";
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", auth_router);
-app.use("/api/finance", financeRoutes);
+app.use("/api/finance", protect, financeRoutes);
 app.use("/api", expense_router);
 app.use("/api/profile", profile_router);
 
